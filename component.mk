ee1b472ae4e3d8bcfceb9f869e6d0e157f13fb38
Daniel <inxonic@localhost>
2017-04-27 22:33:16 +0200

    make some variables overridable

--- component.mk
+++ component.mk
@@ -1,9 +1,14 @@
-MCUFONT = mcufont/encoder/mcufont
+MCUFONT ?= $(MCUFONT_INTERNAL)
 
-MF_FONTS_DIR = $(COMPONENT_PATH)/mcufont/fonts
+MF_FONTS_DIR ?= $(MF_FONTS_DIR_INTERNAL)
 
-MF_FONTS = DejaVuSans12 DejaVuSans12bw DejaVuSerif16 DejaVuSerif32 \
+MF_FONTS ?= DejaVuSans12 DejaVuSans12bw DejaVuSerif16 DejaVuSerif32 \
 	fixed_5x8 fixed_7x14 fixed_10x20 DejaVuSans12bw_bwfont
+
+
+MCUFONT_INTERNAL = $(COMPONENT_PATH)/mcufont/encoder/mcufont
+
+MF_FONTS_DIR_INTERNAL = $(COMPONENT_PATH)/mcufont/fonts
 
 
 MFDIR = mcufont/decoder
@@ -12,20 +17,22 @@ include $(COMPONENT_PATH)/$(MFDIR)/mcufont.mk
 
 COMPONENT_OBJS = $(patsubst %.c,%.o,$(MFSRC))
 
+
 COMPONENT_ADD_INCLUDEDIRS = $(MFDIR)
 COMPONENT_SRCDIRS = $(MFDIR) mcufont
 COMPONENT_EXTRA_INCLUDES = $(MF_FONTS_DIR)
 
 
-$(MFDIR)/mf_font.o:	$(BUILD_DIR_BASE)/include/fonts.h
+$(MFDIR)/mf_font.o:	$(BUILD_DIR_BASE)/include/fonts.h $(MF_FONTS_SRC)
 
 MF_FONTS_SRC = $(patsubst %,$(MF_FONTS_DIR)/%.c,$(MF_FONTS))
+MF_FONTS_SRC_INTERNAL = $(patsubst %,$(MF_FONTS_DIR_INTERNAL)/%.c,$(MF_FONTS))
 
 $(BUILD_DIR_BASE)/include/fonts.h:	$(MF_FONTS_SRC)
 	/bin/echo -e $(foreach font,$(MF_FONTS),'\n#include "'$(font)'.c"') > $@
 
-$(MF_FONTS_SRC):	$(MCUFONT)
-	make -C $(COMPONENT_PATH)/mcufont/fonts
+$(MF_FONTS_SRC_INTERNAL):	$(MCUFONT)
+	make -C $(COMPONENT_PATH)/mcufont/fonts MCUFONT=$(MCUFONT)
 
-$(MCUFONT):
+$(MCUFONT_INTERNAL):
 	make -C $(COMPONENT_PATH)/mcufont/encoder
